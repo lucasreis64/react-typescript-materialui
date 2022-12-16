8ba5a5f0b6950d19f0cabc6d5cdf519203e99542
Lucas <lucasreis64@gmail.com>
2022-12-15 23:07:47 -0300

    feat: material ui and light theme

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,9 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/main-page" element={<p>Main Page</p>} />
+            <Route path="/main-page" element={<Button variant='contained' color='primary'>Main Page</Button>} />
             
             <Route path="*" element={<Navigate to="/main-page" />} />
         </Routes>
